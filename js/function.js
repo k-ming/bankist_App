15ae3c51efd469c3a20d392ca1370fde81e1a4aa
k-ming <keiramingj@gmail.com>
2021-04-20 15:52:06 +0900

    working on it

---
+++ js/function.js
@@ -0,0 +1,10 @@
+//elements 
+const btn = document.querySelector('.login-box');
+const idInput = document.querySelector('.id');
+const pwInput = document.querySelector('.pw');
+const h = document.querySelector('h1');
+
+const ID = 'jenny';
+const PW = 007;
+
+
